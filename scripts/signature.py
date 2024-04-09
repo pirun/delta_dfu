c6e2181557d3987ec9c4605b0b3efaa6f9728ed4
Noy Luo <noy.luo@nordicsemi.no>
2024-04-09 10:40:57 +0800

    update Makefile to support 9160DK

--- scripts/signature.py
+++ scripts/signature.py
@@ -1,2 +1,2 @@
-"python  C:/NCS_SDK/v2.5.0/bootloader/mcuboot/scripts/imgtool.py  sign --key C:/NCS_SDK/v2.5.0/bootloader/mcuboot/root-ec-p256.pem --header-size 0x200 --align 4 --version 0.0.0+0 --pad-header --slot-size 0xaf000"
+"python  C:/NCS_SDK/v2.5.0/bootloader/mcuboot/scripts/imgtool.py  sign --key C:/NCS_SDK/v2.5.0/bootloader/mcuboot/root-ec-p256.pem --header-size 0x200 --align 4 --version 0.0.0+0 --pad-header --slot-size 0xa0000"
 
